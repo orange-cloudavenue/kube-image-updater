1dd8c4fb30708659d3a23fad4c406b0d1074a6ad
dmicheneau <47741512+dmicheneau@users.noreply.github.com>
2024-10-20 23:14:07 +0200

    chore: Add new row for CRD Images in get kubectl cmd

--- internal/controller/image_controller.go
+++ internal/controller/image_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"time"
 
 	"github.com/sirupsen/logrus"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -82,20 +83,26 @@ func (r *ImageReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 		r.Recorder.Event(&image, "Normal", string(ImageUpdate), "Image configuration has changed. Reloading image.")
 	}
 
+	// Set the status time YYYY-MM-DDTHH:MM:SSZ
+	image.SetStatusTime(time.Now().Format(time.RFC3339))
+
 	if an.CheckSum().IsNull() || !equal {
 		if err := an.CheckSum().Set(image.Spec); err != nil {
 			xlog.WithError(err).Error("unable to set checksum")
+			image.SetStatusResult("Unable to set checksum")
 			return ctrl.Result{}, err
 		}
 		if err := r.Client.Update(ctx, &image); err != nil {
 			xlog.WithError(err).Error("unable to update Image")
+			image.SetStatusResult("Unable to update Image")
 			return ctrl.Result{}, err
 		}
 	}
 
 	// * Status
-
 	image.SetStatusTag(an.Tag().Get())
+	image.SetStatusResult("Success")
+
 	if err := r.Status().Update(ctx, &image); err != nil {
 		xlog.WithError(err).Error("unable to update Image status")
 		return ctrl.Result{}, err
