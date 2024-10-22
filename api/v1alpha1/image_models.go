44334083518b754ee4942ff03c21e6d4d6310a00
dmicheneau <47741512+dmicheneau@users.noreply.github.com>
2024-10-22 18:00:26 +0200

    doc: Add crd images

--- api/v1alpha1/image_models.go
+++ api/v1alpha1/image_models.go
@@ -5,25 +5,30 @@ type (
 )
 
 var (
-	// ImageStatusLastSyncSuccess is the status of the image when it is last sync success
+	// ImageStatusLastSyncSuccess is the status of the image when it is last sync success.
 	ImageStatusLastSyncSuccess ImageStatusLastSync = "Success"
 
-	// ImageStatusLastSyncScheduled is the status of the image when it is last sync is scheduled
+	// ImageStatusLastSyncScheduled is the status of the image when it is last sync is scheduled.
 	ImageStatusLastSyncScheduled ImageStatusLastSync = "Scheduled"
 
-	// ERROR STATUS
-	// ImageStatusError is the status of the image when an error occurred
+	// ImageStatusError is the status of the image when an error occurred.
 	ImageStatusLastSyncError ImageStatusLastSync = "Error"
-	// ImageStatusLastSyncGetError is the status of the image when it is last sync get error
+
+	// ImageStatusLastSyncGetError is the status of the image when it is last sync get error.
 	ImageStatusLastSyncErrorGetImage ImageStatusLastSync = "GetImageError"
-	// ImageStatusLastSyncErrorSecrets is the status of the image when it is last sync error secrets
+
+	// ImageStatusLastSyncErrorSecrets is the status of the image when it is last sync error secrets.
 	ImageStatusLastSyncErrorPullSecrets ImageStatusLastSync = "PullSecretsError"
-	// ImageStatusLastSyncErrorRegistry is the status of the image when it is last sync error registry
+
+	// ImageStatusLastSyncErrorRegistry is the status of the image when it is last sync error registry.
 	ImageStatusLastSyncErrorRegistry ImageStatusLastSync = "RegistryError"
-	// ImageStatusLastSyncErrorTags is the status of the image when it is last sync error tags
+
+	// ImageStatusLastSyncErrorTags is the status of the image when it is last sync error tags.
 	ImageStatusLastSyncErrorTags ImageStatusLastSync = "TagsError"
-	// ImageStatusLastSyncErrorGetRule is the status of the image when it is last sync error get rule
+
+	// ImageStatusLastSyncErrorGetRule is the status of the image when it is last sync error get rule.
 	ImageStatusLastSyncErrorGetRule ImageStatusLastSync = "GetRuleError"
-	// ImageStatusLastSyncErrorAction is the status of the image when it is last sync error action
+
+	// ImageStatusLastSyncErrorAction is the status of the image when it is last sync error action.
 	ImageStatusLastSyncErrorAction ImageStatusLastSync = "ActionError"
 )
