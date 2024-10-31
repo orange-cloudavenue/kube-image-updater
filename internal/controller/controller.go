3bd2ff11dc4c01d2ae8a2e0a1b00b494a8174f02
david micheneau <47741512+dmicheneau@users.noreply.github.com>
2024-10-31 15:37:08 +0100

    chore: fix rbac role (#91)
    Co-authored-by: David MICHENEAU <david.micheneau@orange.com>

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,3 +1,4 @@
 package controller
 
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;watch;create;update;patch
+//+kubebuilder:rbac:groups=admissionregistration.k8s.io,resources=mutatingwebhookconfigurations,verbs=get;list;watch;create;update;patch;delete
